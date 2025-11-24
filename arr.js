92e01dc017bbce330dd823806bbfe03bd2173be8
shivam-k1029 <yarshivam57@gmail.com>
2025-11-24 13:31:23 +0530

    bla bla

--- arr.js
+++ arr.js
@@ -4,3 +4,7 @@ console.log(arr); // Logging the entire array
 console.log(arr.length); // Length of the array
 
 console.log(arr[1]); // Accessing the second element ('Hello')
+
+
+arr.push('New Element'); // Adding an element at the end
+console.log(arr);
