4220122d699be4fac6353a7f5bac96ac34492db6
shivam-k1029 <yarshivam57@gmail.com>
2025-12-02 10:49:46 +0530

    attr added

--- dos.js
+++ dos.js
@@ -2,5 +2,26 @@
 // ____________ClassName(' ')
 // ____________Id(' ')
 //  querySeclector('.A') for class-->(.) for id-->(#)
+//  querySeclectorAll('.B');   If more than 1 class and we have implement all of them, then this tag will be use.
 
- 
+let changeText = document.getElementById('user-greeting')
+console.log(changeText)
+
+changeText.textContent = "Bla Bla "  // textContent will change the text inside the tag.
+
+let paragraph1 = document.querySelector('p')
+console.log(paragraph1)
+paragraph1.innerHTML = "<u>This is paragraph one changed by JS.</u>" // innerHTML will change the whole HTML inside the tag.
+
+
+//value in input tag
+const Welcome = document.getElementById('welcome');
+
+const nameInput = document.getElementById('name');
+
+nameInput.value = "Shipra" // changing the value of input tag using JS.
+console.log(nameInput.value);
+
+
+Welcome.style.fontSize = "24px"; // changing the style of the tag using JS.
+Welcome.style.color = "blue"; // changing the color of the tag using JS.
--- loop.js
+++ loop.js
@@ -1,4 +1,37 @@
-// 
-for(i=0; i<10; i++){
-    console.log(i)
+// // 
+// for(i=0; i<10; i++){
+//     console.log(i)
+        // if(i==5){
+        //     break; // terminates the loop when i is 5
+        // }
+// }
+
+
+// // while loop
+
+// let k = 0 
+
+// while(k<4){
+//     console.log(k) // condition
+//     k++;
+// }
+
+
+// // do...while Loop
+
+// let num = 0
+
+// do{
+//     console.log(num) // at least one time will be executed
+//     num++;
+// }while(num<5) // condition
+
+let obj = {
+    name: "John",
+    age: 30,
+    city: "New York"
 }
+
+for (key in obj){
+    console.log(key + ": " + obj[key])
+}   
